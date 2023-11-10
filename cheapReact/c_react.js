3db6c5dfc7cc865276a61eb902f797b37ee19cd0
Stradex <stradexengine@gmail.com>
2023-11-10 11:38:30 -0300

    OK: Having a basic DOM Tree generated

--- cheapReact/c_react.js
+++ cheapReact/c_react.js
@@ -1,3 +1,65 @@
-export function r_drawTree(component, children) {
-    return "<div></div>";
+/*
+ A component can be:
+    -> Function
+    -> String
+*/
+
+let ComponentsTree = [];
+
+function treeObjToDOMString(componentObj) {
+    let DOMString = "";
+    if (componentObj.tag && componentObj.tag === "_text_") {
+        return componentObj.children;
+    }
+    
+    if (componentObj.tag) {
+        DOMString += "<" + componentObj.tag + ">\n";
+    }
+
+    if (componentObj.children) {
+        for (let i=0; i < componentObj.children.length; i++) {
+            DOMString += treeObjToDOMString(componentObj.children[i]);
+        }
+    }
+
+    if (componentObj.tag) {
+        DOMString += "\n</" + componentObj.tag + ">";
+    }
+    return DOMString;
+}
+
+function r_getStringComponent(strComponent) {
+    return {tag: "_text_", children: strComponent}
+}
+
+function r_getFunctionComponent(funcComponent) {
+    return funcComponent();
+}
+
+function r_draw(component) {
+    let objComponent;
+    switch (typeof component) {
+        case "function":
+            objComponent = r_getFunctionComponent(component);
+        break;
+        case "string":
+            objComponent = r_getStringComponent(component);
+        break;
+        case "object":
+            objComponent = component;
+        break;
+    }
+
+    if (!objComponent.children || typeof objComponent.children === "string") return objComponent;
+
+    for (let i=0; i < objComponent.children.length; i++) {
+        objComponent.children[i] = r_draw(objComponent.children[i]);
+    }
+    
+    return objComponent;
+}
+
+export function r_main(mainComponent) {
+    let DOMTree = r_draw(mainComponent);
+    return treeObjToDOMString(DOMTree);
 }
--- s-2.js
+++ s-2.js
@@ -1,3 +1,40 @@
 import * as CReact from "./cheapReact/c_react.js";
 
-console.log(CReact);
+
+function C4() {
+    return {
+        tag: "span",
+        children: ["This is my span text"]
+    }
+}
+
+
+function C3() {
+    return {
+        tag: "p",
+        children: [
+            "This is my text: ",
+            C4
+        ]
+    }
+}
+
+function C2() {
+    return {
+        tag: "h1",
+        children: ["This is my title"]
+    }
+}
+
+function C1() {
+    return {
+        tag: "div",
+        children: [
+            C2,
+            C3
+        ]
+    }
+}
+
+let DOMString = CReact.r_main(C1);
+console.log(DOMString);
